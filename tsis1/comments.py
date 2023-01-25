46511d3421b35a6936a3bfaf91a32049fb04a2cc
ddilnaz~ <d_zeinolla@kbtu.kz>
2023-01-25 22:42:29 +0600

    First commit

---
+++ tsis1/comments.py
@@ -0,0 +1,25 @@
+#comment
+print("Hello, World!")
+
+
+print("Hello, World!") #comment
+
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello")
+
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello")
